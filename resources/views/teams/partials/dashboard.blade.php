49f939300db29e38ef759ec3c24bee3f0b5613f1
Al Solis <absolis2@up.edu.ph>
2026-03-28 16:55:14 +0800

    update

--- resources/views/teams/partials/dashboard.blade.php
+++ resources/views/teams/partials/dashboard.blade.php
@@ -16,7 +16,7 @@
         <div class="bg-white border border-gray-200 rounded-lg shadow h-fill flex flex-col">
             <div class="{{ $style['bg'] }} text-white p-4 rounded-t-lg flex justify-between">
                 <h5 class="font-bold">{{ $status }}</h5>
-                <span class="bg-gray-100  text-xs text-bold text-blue-900 font-semibold px-2.5 py-0.5 rounded">
+                <span class="bg-gray-100 text-xs {{ $style['text'] }} font-semibold px-2.5 py-0.5 rounded">
                     {{ $tickets->where('status', $status)->count() }}
                 </span>
             </div>
